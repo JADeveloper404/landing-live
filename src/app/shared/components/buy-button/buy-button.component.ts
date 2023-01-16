49a2851cf037db95144e6327e4702899e43e2af3
Jesus Arevalo <jarevalo@saludelectronica.com>
2023-01-15 19:45:01 -0500

    Responsive first section y menu

--- src/app/shared/components/buy-button/buy-button.component.ts
+++ src/app/shared/components/buy-button/buy-button.component.ts
@@ -8,6 +8,7 @@ import { Component, Input, OnInit } from '@angular/core';
 export class BuyButtonComponent implements OnInit {
 
   @Input() titleButton = "Comprar curso";
+  @Input() hrefButton = "#";
 
   constructor() { }
 
