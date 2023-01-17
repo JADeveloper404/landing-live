076d67ed65bf389a2a97dae273d9a9d148771b16
Jesus Arevalo <jarevalo@saludelectronica.com>
2023-01-17 10:56:41 -0500

    Ajustes imagenes

--- src/app/landing/components/third-section/third-section.component.ts
+++ src/app/landing/components/third-section/third-section.component.ts
@@ -9,23 +9,23 @@ export class ThirdSectionComponent implements OnInit {
 
   dataList = [
     {
-      src: "../../../../assets/img/iconos/check.png",
+      src: "assets/img/iconos/check.png",
       text: "Sabrás cuáles son los 7 propósitos para vivir una vida plena.",
     },
     {
-      src: "../../../../assets/img/iconos/check.png",
+      src: "assets/img/iconos/check.png",
       text: "Despertarás el deseo de disfrutar y aprovechar cada día que tienes por vivir.",
     },
     {
-      src: "../../../../assets/img/iconos/check.png",
+      src: "assets/img/iconos/check.png",
       text: "Comenzarás a encaminar tu vida hacia la plenitud y verdadera felicidad.",
     },
     {
-      src: "../../../../assets/img/iconos/check.png",
+      src: "assets/img/iconos/check.png",
       text: "Inspirarás a otras personas a vivir desde el propósito.",
     },
     {
-      src: "../../../../assets/img/iconos/check.png",
+      src: "assets/img/iconos/check.png",
       text: "Finalmente, aportarás a la construcción de un mundo mejor.",
     },
   ]
